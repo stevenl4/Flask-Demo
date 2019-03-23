e67904c38c894c3c268374f52676b68776dd3613
stevenl4 <steven.y.luo@gmail.com>
2019-03-23 13:05:30 -0400

    add profile and shopping page

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'This is the homepage'
+@app.route('/<username>')
+def index(username=None):
+    return render_template('user.html', username=username)
+
+
+@app.route('/bacon', methods=['GET', 'POST'])
+def bacon():
+    if request.method == 'POST':
+        return 'You are using POST'
+    else:
+        return 'You are using GET'
+
+
+@app.route("/shopping")
+def shopping():
+    food = ["Cheese", "Tuna", "Beef"]
+    return render_template("shopping.html", food=food)
 
 
 @app.route('/tuna')
+def tuna():
+    return '<h2>Tuna is good</h2>'
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    return render_template("profile.html", username=username)
+
+
+
 
 
 if __name__ == '__main__':
